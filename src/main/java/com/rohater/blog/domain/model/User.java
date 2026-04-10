ceeae933ec99860863cf75f5a30639a37eda3782
RohatErgun <rohatergun06@gmail.com>
2026-04-10 16:05:11 +0200

    add onCreate method to populate createAt variable Prepersist

--- src/main/java/com/rohater/blog/domain/model/User.java
+++ src/main/java/com/rohater/blog/domain/model/User.java
@@ -46,4 +46,9 @@ public class User {
     public int hashCode() {
         return Objects.hash(id, email, password, name, createdAt);
     }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 }
