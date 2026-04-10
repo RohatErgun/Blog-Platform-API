d18c8672c93ae21b12d32242ac9aede4b1a67b49
RohatErgun <rohatergun06@gmail.com>
2026-04-10 16:24:10 +0200

    add Post.java for blogPost content

---
+++ src/main/java/com/rohater/blog/domain/model/Post.java
@@ -0,0 +1,72 @@
+package com.rohater.blog.domain.model;
+
+import com.rohater.blog.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Post post = (Post) object;
+        return Objects.equals(id, post.id)
+                && Objects.equals(title, post.title)
+                && Objects.equals(content, post.content)
+                && status == post.status
+                && Objects.equals(readingTime, post.readingTime)
+                && Objects.equals(createdAt, post.createdAt)
+                && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, status, readingTime, createdAt, updatedAt);
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now; // when the user create post, current time is initial updatedAt
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
