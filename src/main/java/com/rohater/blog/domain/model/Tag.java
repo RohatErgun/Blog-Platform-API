8078ef78da897d0a559ce630def18e2a983295c5
RohatErgun <rohatergun06@gmail.com>
2026-04-10 16:10:11 +0200

    add Tag.java

--- src/main/java/com/rohater/blog/domain/model/Tag.java
+++ src/main/java/com/rohater/blog/domain/model/Tag.java
@@ -1,4 +1,36 @@
 package com.rohater.blog.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tags")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Tag tag = (Tag) object;
+        return Objects.equals(id, tag.id) && Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
