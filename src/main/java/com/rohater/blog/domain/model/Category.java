93f572e9d4f8378ccd205336c81323b839753339
RohatErgun <rohatergun06@gmail.com>
2026-04-10 16:08:11 +0200

    add Category.java

--- src/main/java/com/rohater/blog/domain/model/Category.java
+++ src/main/java/com/rohater/blog/domain/model/Category.java
@@ -1,4 +1,36 @@
 package com.rohater.blog.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Category category = (Category) object;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
