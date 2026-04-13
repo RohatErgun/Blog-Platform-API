43e31abc48c2b7b93350bb47f92bbbadae34d66a
RohatErgun <rohatergun06@gmail.com>
2026-04-13 10:59:28 +0200

    add CatagoryDTO and define CategoryController

---
+++ src/main/java/com/rohater/blog/controllers/CategoryController.java
@@ -0,0 +1,21 @@
+package com.rohater.blog.controllers;
+
+import com.rohater.blog.domain.model.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories ")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> listCategories(){
+        // Todo
+    }
+}
