96d78dda3432059907fb0e937e7f7fb01c49011b
RohatErgun <rohatergun06@gmail.com>
2026-04-14 12:33:34 +0200

    feat: add deleteCategory method

--- src/main/java/com/rohater/blog/services/CategoryService.java
+++ src/main/java/com/rohater/blog/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.rohater.blog.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.rohater.blog.domain.model.Category;
 
 public interface CategoryService {
-
     List<Category> listCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
 }
