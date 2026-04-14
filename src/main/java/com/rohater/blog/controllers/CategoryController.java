64760fee3da30a802ffcdad47dcdf74feff83c7b
RohatErgun <rohatergun06@gmail.com>
2026-04-14 12:32:03 +0200

    feat: define deleteCategory endpoint

--- src/main/java/com/rohater/blog/controllers/CategoryController.java
+++ src/main/java/com/rohater/blog/controllers/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.rohater.blog.domain.dtos.CategoryDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -43,5 +44,10 @@ public class CategoryController {
           categoryMapper.toDTo(savedCategory),
                 HttpStatus.CREATED
         );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteCategory(@PathVariable UUID id){
+        // Todo
     }
 }
