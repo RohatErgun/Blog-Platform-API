c3caaec15795f1f858dfd211dd09dcf083de58e3
RohatErgun <rohatergun06@gmail.com>
2026-04-14 10:52:58 +0200

    feat: add handleException and handleIllegalArgumentException methods

--- src/main/java/com/rohater/blog/controllers/ErrorController.java
+++ src/main/java/com/rohater/blog/controllers/ErrorController.java
@@ -1,10 +1,13 @@
 package com.rohater.blog.controllers;
 
+import com.rohater.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @ControllerAdvice
@@ -12,7 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorController {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity handleException(Exception e){
-        // Todo
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e){
+        log.error("Caught Exception", e);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException e){
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("Invalid Request")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
