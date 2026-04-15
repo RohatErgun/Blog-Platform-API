d64ced2af0b67640f5ae048d7de234f48b4ed184
RohatErgun <rohatergun06@gmail.com>
2026-04-15 16:45:53 +0200

    feat: add authenticate method

--- src/main/java/com/rohater/blog/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/rohater/blog/services/impl/AuthenticationServiceImpl.java
@@ -2,7 +2,9 @@ package com.rohater.blog.services.impl;
 
 import com.rohater.blog.security.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private AuthenticationManager authenticationManager;
     private UserDetailsService userDetailsService;
 
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private Long jwtExpiryMs = 86400000L; // 24 hours
 
     @Override
     public UserDetails authenticate(String email, String password) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        return userDetailsService.loadUserByUsername(email);
     }
 
     @Override
