78006da248514579a759dc5e0f02638db3304922
RohatErgun <rohatergun06@gmail.com>
2026-04-15 13:49:59 +0200

    feat: add BlogUserDetailsService

---
+++ src/main/java/com/rohater/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.rohater.blog.security;
+
+
+import com.rohater.blog.domain.model.User;
+import com.rohater.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+         User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found " + email)
+        );
+
+        return new BlogUserDetails(user);
+    }
+}
