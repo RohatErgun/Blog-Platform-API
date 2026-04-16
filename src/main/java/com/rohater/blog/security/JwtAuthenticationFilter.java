09724a1cedec69256b3361c296dffffd7731d06c
RohatErgun <rohatergun06@gmail.com>
2026-04-16 17:06:27 +0200

    feat: add AuthenticationFilter.java

---
+++ src/main/java/com/rohater/blog/security/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.rohater.blog.security;
+
+import com.rohater.blog.services.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationService authenticationService;
+
+    @Override
+    protected void doFilterInternal
+            (HttpServletRequest request,
+             HttpServletResponse response,
+             FilterChain filterChain) throws ServletException, IOException {
+        String token = extractToken(request);
+        if (token != null){
+            UserDetails userDetails = authenticationService.validateToken(token);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            // setting userId to not check for email all the time
+            if (userDetails instanceof BlogUserDetails){
+                request.setAttribute("userId", ((BlogUserDetails) userDetails.getId()));
+            }
+        }
+
+    }
+
+    private String extractToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
