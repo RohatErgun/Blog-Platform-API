3c684116f716f9b5d3ef0889412491d4c104f120
RohatErgun <rohatergun06@gmail.com>
2026-04-17 16:41:37 +0200

    feat: add register method to AuthenticationService interface

--- src/main/java/com/rohater/blog/services/AuthenticationService.java
+++ src/main/java/com/rohater/blog/services/AuthenticationService.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface AuthenticationService {
     UserDetails authenticate(String email, String password);
+    UserDetails register(String name, String email, String password);
     String generateToken(UserDetails userDetails);
     UserDetails validateToken(String token);
 }
